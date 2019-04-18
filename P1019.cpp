cdf6d00141fa7c2fda3227d697d88dc34e03b6da
maidangdang1 <46847241+maidangdang1@users.noreply.github.com>
2019-04-18 09:29:10 +0000

    temp 1019

--- P1019.cpp
+++ P1019.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int n;
 int maxi = 0;
-string ans[n * 2 + 10];
+string ans[50];
+int help[50];
 
 struct node
 {
@@ -15,8 +16,9 @@ struct node
     int data = 2;
 };
 
-bool check()
+bool check(int num)
 {
+    /*
     int i;
     int len;
     for (i = 1;i < n * 2;i++)
@@ -26,6 +28,47 @@ bool check()
             return false;
     }
     return true;
+    */
+    int i;
+    /*
+    string temp1;
+    string temp2;
+    */
+    int len1 = ans[num].ss.length();
+    int len2 = ans[num + 1].ss.length();
+    /*
+    if (len1 < len2)
+    {
+        //strncpy(temp1, ans[num].ss)
+        for (i = len1 - 1;i > 0;i--)
+        {
+            if (ans[num].ss[i] == ans[num + 1].ss[0])
+            {
+                help[num] = len1 - i;
+                return true;
+            }
+        }
+    }
+    else
+    {
+        for (i = len2 - 1;i > 0;i--)
+        {
+            if (ans[num + 1].ss[i] == ans[num + 1].ss[0])
+            {
+                help[num] = len1 - i;
+                return true;
+            }
+        }
+    }
+    */
+   for (i = len1 - 1;i > 0;i--)
+    {
+        if (ans[num].ss[i] == ans[num + 1].ss[0] && len1 - i != )
+        {
+            help[num] = len1 - i;
+            return true;
+        }
+    }
 }
 
 void dfs(int floor)
@@ -33,9 +76,9 @@ void dfs(int floor)
     int i;
     if (floor > n * 2)
     {
-        if (check())
+        for (i = 1;i < n * 2;i++)
         {
-
+            if (check(i))
         }
     }
     else
