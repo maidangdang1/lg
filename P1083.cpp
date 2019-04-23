23e9ba55b2aba0a6c96bf6209c7653ab5b32529c
maidangdang1 <46847241+maidangdang1@users.noreply.github.com>
2019-04-23 09:24:34 +0000

    1083

--- P1083.cpp
+++ P1083.cpp
@@ -18,12 +18,12 @@ int n, m;
 int r[INF];
 node order[INF];
 int cf[INF];
-int copy[INF];
+int copyed[INF];
 
 bool check(int ans)
 {
     memset(cf, 0, sizeof(cf));
-    memcpy(copy, r, sizeof(copy));
+    memcpy(copyed, r, sizeof(r));
     int i;
     for (i = 1;i <= ans;i++)
     {
@@ -33,8 +33,8 @@ bool check(int ans)
     for (i = 1;i <= n;i++)
     {
         cf[i] += cf[i - 1];
-        copy[i] += cf[i];
-        if (copy[i] < 0)
+        copyed[i] += cf[i];
+        if (copyed[i] < 0)
             return false;
     }
     return true;
@@ -42,6 +42,7 @@ bool check(int ans)
 
 int main()
 {
+    freopen("test.txt", "r", stdin);
     //int n, m;
     cin >> n >> m;
     //int r[n + 10];
@@ -52,7 +53,7 @@ int main()
     for (i = 1;i <= m;i++)
         cin >> order[i].d >> order[i].s >> order[i].t;
     //int cf[n + 10];
-    //int copy[n + 10];
+    //int copyed[n + 10];
     //memset(cf, 0, sizeof(cf));
     int l = 1;
     int r = m;
@@ -60,6 +61,15 @@ int main()
     while (l < r)
     {
         mid = (l + r) >> 1;
-        if (check())
+        if (check(mid))
+            l = mid + 1;
+        else
+            r = mid;
     }
+    if (l == m && check(mid))
+        cout << 0;
+    else
+        cout << -1 << endl << l;
+    fclose(stdin);
+    return 0;
 }
