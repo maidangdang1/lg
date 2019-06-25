3cbbc406b76b7beb6e08fdd1772beba98176c915
maidangdang1 <46847241+maidangdang1@users.noreply.github.com>
2019-06-25 17:27:29 +0800

    Update P1280.cpp

--- P1280.cpp
+++ P1280.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,14 +9,20 @@ struct node
     int t;
 };
 
+bool comp(node x, node y)
+{
+    return x.p < y.p;
+}
+
 int main()
 {
     int n;
     int k;
     cin >> n >> k;
-    node data[n + 10];
+    node data[k + 10];
     int i;
-    for (i = 1;i <= n;i++)
+    for (i = 1;i <= k;i++)
         cin >> data[i].p >> data[i].t;
-    
+    sort(data + 1, data + 1 + k, comp);
+    int help
 }
