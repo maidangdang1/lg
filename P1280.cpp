ed2b6d068c1b5fdc191d096f2e606d746b6c910a
maidangdang1 <46847241+maidangdang1@users.noreply.github.com>
2019-06-29 11:47:34 +0800

    Update P1280.cpp

--- P1280.cpp
+++ P1280.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -9,20 +12,38 @@ struct node
     int t;
 };
 
-bool comp(node x, node y)
-{
-    return x.p < y.p;
-}
-
 int main()
 {
-    int n;
-    int k;
+    freopen("test.txt", "r", stdin);
+    int n, k;
     cin >> n >> k;
-    node data[k + 10];
+    //node task[k + 10];
     int i;
+    /*
     for (i = 1;i <= k;i++)
-        cin >> data[i].p >> data[i].t;
-    sort(data + 1, data + 1 + k, comp);
-    int help
+        cin >> task[i].p >> task[i].t;
+    */
+    vector <int> task[n + 10];
+    int p, t;
+    for (i = 1;i <= k;i++)
+    {
+        cin >> p >> t;
+        task[p].push_back(t);
+    }
+    int rest[n + 10];
+    vector <int>::iterator it;
+    memset(rest, 0, sizeof(rest));
+    for (i = n;i > 0;i--)
+    {
+        if (task[i].empty())
+            rest[i] = rest[i + 1] + 1;
+        else
+        {
+            for (it = task[i].begin();it != task[i].end();it++)
+                rest[i] = max(rest[i], rest[i + *it]);
+        }
+    }
+    cout << rest[1];
+    fclose(stdin);
+    return 0;
 }
