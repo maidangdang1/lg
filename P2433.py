2aef3d29019f85bfc7aee0fd0bf6985f90ba9846
maidangdang1 <46847241+maidangdang1@users.noreply.github.com>
2020-08-05 16:46:27 +0800

    Create P2433.py

---
+++ P2433.py
@@ -0,0 +1,32 @@
+temp = input().split()
+t = int(temp[0])
+if t == 1:
+    print('I love Luogu!')
+elif t == 2:
+    print('6 4')
+elif t == 3:
+    print('3 12 2')
+elif t == 4:
+    print('%.3f' %(500 / 3))
+elif t == 5:
+    print('15')
+elif t == 6:
+    print(117 ** 0.5)
+elif t == 7:
+    print('110  90  0')
+elif t == 8:
+    PI = 3.141593
+    print(PI * 10, PI * 25, 4 * PI * 125 / 3, sep = '\n')
+elif t == 9:
+    print('22')
+elif t == 10:
+    print('9')
+elif t == 11:
+    print(100 / 3)
+elif t == 12:
+    print(ord('M') - 64, chr(64 + 18), sep = '\n')
+elif t == 13:
+    PI = 3.141593
+    print('%d' %((4256 * PI / 3) ** (1.0 / 3)))
+elif t == 14:
+    print('50')
