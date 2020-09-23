a56171e68a59adac21809ea046524ca25976b68f
maidangdang1 <mmt869601840@gmail.com>
2020-09-23 23:19:43 +0800

    P2142.cpp

---
+++ P2142.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    string temp;
+    cin >> temp;
+    
+}
